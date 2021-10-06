1dcac3d2695c9b552ea510d3569317499cbaab34
Shawn Kebel <fxrender@outlook.com>
2021-10-06 12:35:30 -0500

    use filter to return only emails with 'yahoo.com', ending

--- app/index.js
+++ app/index.js
@@ -2,3 +2,5 @@
 
   // Map over array and grab 'vehicles' to destructure. Then use sort to get 'each' persons highest milage, then using sort again to get highest milage in 'all' persons (in whole array).
 // TODO: WHAT IS TOTAL MILEAGE OF ALL VEHICLES?
+
+
