97485931dbd6b44f246329e717c883f174970e66
Shawn Kebel <fxrender@outlook.com>
2021-10-06 12:32:44 -0500

    use .map amd .sort to get highest vehicle mileage

--- app/index.js
+++ app/index.js
@@ -1,1 +1,3 @@
 // TODO: WHICH VEHICLE HAS THE HIGHEST MILEAGE?
+
+  // Map over array and grab 'vehicles' to destructure. Then use sort to get 'each' persons highest milage, then using sort again to get highest milage in 'all' persons (in whole array).
